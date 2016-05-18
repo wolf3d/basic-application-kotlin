3902c16d825161080a02cef51cbf71f137676e3b
wolf <wolf@sloth>
2016-05-18 20:34:41 +0300

    Build an Intent: first steps

---
+++ app/src/main/java/com/github/wolf3d/basicapplication/DisplayMessageActivity.kt
@@ -0,0 +1,7 @@
+package com.github.wolf3d.basicapplication
+
+/**
+ * Created by wolf on 5/18/16.
+ */
+class DisplayMessageActivity {
+}
--- app/src/main/java/com/github/wolf3d/basicapplication/MainActivity.kt
+++ app/src/main/java/com/github/wolf3d/basicapplication/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.github.wolf3d.basicapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.jvm.javaClass
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        public val EXTRA_MESSAGE: String = "com.github.wolf3d.basicapplication.Message"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +20,14 @@ class MainActivity : AppCompatActivity() {
     }
     fun sendMessage(view: View) {
         Log.d("MainActivity","button_send pressed")
+
+        val intent = Intent(MainActivity@this, DisplayMessageActivity::class.java)
+        val editText = edit_message
+
+        val message = editText.text
+        intent.putExtra(EXTRA_MESSAGE,message)
+
+        startActivity(intent)
+
     }
 }
