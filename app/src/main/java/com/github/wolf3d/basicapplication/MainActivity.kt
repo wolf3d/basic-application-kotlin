1eff1d129682db6805d704eb3a29f007dfb7828e
wolf <wolf@sloth>
2016-05-18 19:59:46 +0300

    Starting-Another-Activity: Respond to the Send Button

--- app/src/main/java/com/github/wolf3d/basicapplication/MainActivity.kt
+++ app/src/main/java/com/github/wolf3d/basicapplication/MainActivity.kt
@@ -2,6 +2,8 @@ package com.github.wolf3d.basicapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun sendMessage(view: View) {
+        Log.d("MainActivity","button_send pressed")
     }
 }
